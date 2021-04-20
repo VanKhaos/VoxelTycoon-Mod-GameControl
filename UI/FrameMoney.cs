f76203357b952334f3bebefff99e559d4afa38ff
VanKhaos <xv50k870!?>
2021-04-20 14:56:09 +0200

    add custom money

--- UI/FrameMoney.cs
+++ UI/FrameMoney.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameControl.Util;
+using System;
 using VoxelTycoon;
 using VoxelTycoon.Modding;
 using VoxelTycoon.UI;
@@ -11,10 +12,12 @@ namespace GameControl
         public static Frame ShowFrame()
         {
             Dialog val = Dialog.Create();
+            Action<String> myAction = new Action<String>(a => { });
+
             double money = 500000.00;
 
             val.Text = "Game Control | Money Tab\n\n" +
-                       $"Company Money: {Company.Current.Value}";
+                      $"Company Money: {Company.Current.Value}";
 
             val.AddPositiveButton("+ 500.000", (Action)delegate
             {
@@ -26,10 +29,18 @@ namespace GameControl
                 Company.Current.AddMoney(-money, BudgetItem.Other, true);
             }, false);
 
-            val.AddButton("Close", (Action)delegate
+            val.AddButton("Custom", ColorHelper.FromHexString("041887"), (Action)delegate
             {
-
+                InputDialog input = InputDialog.ShowFor("How much do you want ?\n^\n" +
+                                                        "Max: 1.000.000\n" +
+                                                        "Min: -1.000.000\n" +
+                                                        "Enter without dot (.)", "50000", new Action<String>(a => {
+                                                            GameMoney.CustomMoney(a.ToString()); 
+                                                        }));                 
             }, true);
+
+
+            val.AddButton("Close", (Action)delegate{}, true);
 
             val.Show();
             return (Frame)(object)val;
---
+++ Util/GameMoney.cs
@@ -0,0 +1,19 @@
+﻿using VoxelTycoon;
+using VoxelTycoon.Modding;
+
+namespace GameControl.Util
+{
+    class GameMoney : Mod
+    {
+        public static void CustomMoney(string input)
+        {
+            double doubleVal = 0;
+            doubleVal = double.Parse(input);
+            if(doubleVal < 1000001 || doubleVal > -1000001)
+            {
+                Company.Current.AddMoney(doubleVal, BudgetItem.Other, true);
+            }
+            
+        }
+    }
+}
